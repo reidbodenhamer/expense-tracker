84158cfb89b8221c6e5ebcba38bea42edfeb4c51
Reid Bodenhamer <reidb@ou.edu>
2025-07-28 17:35:49 -0500

    Added login/sign up page - input email & password at login - sign up with full name and optionally a photo - backend logic to be added later

--- client/src/pages/Auth/Login.tsx
+++ client/src/pages/Auth/Login.tsx
@@ -1,9 +1,75 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import Input from "../../components/Inputs/Input";
+import { validateEmail } from "../../utils/helper";
 
-const Login : React.FC = () => {
-    return (
-        <div>Login</div>
-    )
-}
+const Login: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const navigate = useNavigate();
+
+  // handle login form submit
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError("Please enter a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter your password.");
+      return;
+    }
+
+    setError(null);
+
+    // Login api call
+  };
+
+  return (
+    <AuthLayout>
+      <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+        <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
+        <p className="text-xs text-slate-700 mt-[5px] mb-6">
+          Please enter your details to log in
+        </p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+            label="Email Address"
+            placeholder="youremail@example.com"
+            type="text"
+          />
+          <Input
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+            label="Password"
+            placeholder="Enter your password"
+            type="password"
+          />
+
+          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+
+          <button type="submit" className="btn-primary">
+            LOG IN
+          </button>
+
+          <p className="text-[13px] text-slate-800 mt-3">
+            Don't have an account?{" "}
+            <Link className="font-medium text-primary underline" to="/signup">
+              Sign Up
+            </Link>
+          </p>
+        </form>
+      </div>
+    </AuthLayout>
+  );
+};
 
 export default Login;
